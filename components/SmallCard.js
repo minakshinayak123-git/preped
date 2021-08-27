02ea62c726fbb0706b9522a50c4f2dd5779d7b92
minakshiknayak <minakshiknayak@gmail.com>
2021-08-27 22:51:44 +0530

    implemented team member-3

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -8,7 +8,7 @@ const SmallCard = ({
   designation,
 }) => {
   return (
-    <div className='cursor-pointer rounded-lg border-1 shadow-xl hover:-rotate-20 '>
+    <div className='cursor-pointer rounded-lg border-1 shadow-xl'>
       <div className='relative h-80 w-60'>
         <Image
           src={profile_pic}
@@ -17,16 +17,12 @@ const SmallCard = ({
           className='rounded-lg'
         />
         <div className='absolute hover:h-80 w-60 opacity-70 hover:bg-[#007aff] rounded-lg px-4 pt-10 transition-transform transition-shadow-2xl duration-300 ease-out'>
-          <div className='flex flex-wrap flex-col'>
-            <h3 className='hover:text-lg lg:text-xl font-bold text-transparent hover:text-white my-6'>
+          <div className='flex flex-wrap flex-col text-transparent hover:text-white'>
+            <h3 className='hover:text-lg lg:text-xl font-bold my-6'>
               {`${first_name} ${last_name}`}
             </h3>
-            <p className='text-sm text-transparent hover:text-gray-900 font-bold'>
-              {designation}
-            </p>
-            <p className='text-sm text-transparent lg:text-md font-semibold hover:text-white my-6'>
-              {biography}
-            </p>
+            <p className='text-sm font-bold mb-4'>{designation}</p>
+            <p className='text-sm lg:text-md font-semibold'>{biography}</p>
           </div>
         </div>
       </div>
